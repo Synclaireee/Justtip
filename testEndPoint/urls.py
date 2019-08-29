48bf42d49ec65f133e2c3d9dced047671a731192
Synclaireee <Synclaire@users.noreply.github.com>
2019-08-29 11:53:00 +0700

    r

--- testEndPoint/urls.py
+++ testEndPoint/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from testEndPoint import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('s/', views.s_list),
     path('s/<int:pk>/', views.s_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
