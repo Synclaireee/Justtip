e88a788ed564583e172e1ae08dd057d40c3083fd
Synclaireee <Synclaire@users.noreply.github.com>
2019-09-02 23:59:48 +0700

    R URLS

--- Justtip/urls.py
+++ Justtip/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('testEndPoint.urls')),
+    path('s/', include('testEndPoint.urls')),
+    path('r/', include('regtest.urls')),
 ]
--- testEndPoint/urls.py
+++ testEndPoint/urls.py
@@ -3,8 +3,8 @@ from testEndPoint import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('s/', views.S_List.as_view()),
-    path('s/<int:pk>/', views.S_Detail.as_view()),
+    path('', views.S_List.as_view()),
+    path('<int:pk>/', views.S_Detail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
